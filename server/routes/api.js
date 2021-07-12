7a9c91d2c6a6a7d28aa263c2b79d7af128c26595
Danial Hasan <danialhasan14@gmail.com>
2021-07-11 22:43:51 -0400

    added environment variables

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+router.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', `${process.env.ACCESS_CONTROL_ALLOW_ORIGIN}`);
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
+  next()
+})
+
 router.get('/', (req, res) => {
   res.send("This is the root handler.")
 });
--- server/server.js
+++ server/server.js
@@ -4,6 +4,12 @@ const express = require('express');
 // parse env variables
 require('dotenv').config();
 
+if (process.env.NODE_ENV === "development" || process.env.NODE_ENV === undefined) {
+    process.env.ACCESS_CONTROL_ALLOW_ORIGIN = 'http://localhost:3000'
+} else if (process.env.NODE_ENV === "production") {
+    process.env.ACCESS_CONTROL_ALLOW_ORIGIN = 'https://unsplash-clone-dh.netlify.app'
+}
+
 // Configuring port
 const port = process.env.PORT || 9000;
 
@@ -11,6 +17,8 @@ const app = express();
 
 // Configure middlewares
 app.use(express.json());
+
+
 
 app.set('view engine', 'html');
 
