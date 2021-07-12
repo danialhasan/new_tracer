9ef9b6d0387f571f00d4e8438003bfb988582e95
Danial Hasan <danialhasan14@gmail.com>
2021-07-11 22:50:44 -0400

    test commit for netlify

---
+++ client/src/components/DarkModeIcon.vue
@@ -0,0 +1,50 @@
+<script>
+export default {
+  data() {
+    return {
+      icons: {
+        dark: "/assets/sun.svg",
+        light: "/assets/moon-stars-fill.svg",
+      },
+      themeIsDark: "",
+    };
+  },
+  props: [],
+  methods: {
+    toggleTheme() {
+      if (localStorage.theme === "dark") {
+        localStorage.theme = "light";
+        console.log("Theme is now light");
+        document.querySelector("html").classList.remove("dark");
+        this.changeThemeIsDark(false);
+      } else {
+        localStorage.theme = "dark";
+        console.log("Theme is now dark");
+        document.querySelector("html").classList.add("dark");
+        this.changeThemeIsDark(true);
+      }
+      console.log(window.localStorage);
+    },
+    changeThemeIsDark(bool) {
+      this.themeIsDark = bool;
+    },
+  },
+  mounted() {
+    if (localStorage.theme === "dark") {
+      this.changeThemeIsDark(true);
+    } else {
+      this.changeThemeIsDark(false);
+    }
+  },
+};
+</script>
+<template>
+  <div>
+    <img
+      :src="themeIsDark ? this.icons.dark : this.icons.light"
+      alt="Dark Mode Icon"
+      class="w-10 h-10 md:w-12 md:h-12"
+      @click="toggleTheme"
+    />
+  </div>
+</template>
